576b707dbceb4be770337045ea1a3b2f9fe8ce58
areeba ahmad <areeba.ahmadsh18@gmail.com>
2024-04-25 01:46:47 -0700

    dobara changes made

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import UserChat from './userchat';
 import InspectorChat from './inspectorchat';
 
 const App = () => {
-  // Assuming you have a way to determine the user type, such as a state variable or context
-  const isInspector = window.location.pathname === '/inspector'; // Check current URL path
-
   return (
-    <div>
-      {isInspector ? <InspectorChat /> : <UserChat />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/inspector" element={<InspectorChat />} />
+        <Route path="/" element={<UserChat />} />
+      </Routes>
+    </Router>
   );
 };
 
